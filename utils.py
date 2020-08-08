7187f26cafc8ea5d663705e408e1d744995cfa7a
Sgsouham <joe.sg123@gmail.com>
2020-08-08 19:23:39 +0530

    update

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
-import numpy as np
-from torchvision import transforms 
+import torch
 from PIL import Image
+import av
+from torchvision import transforms
+import numpy as np
+
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
 
 
 def load_image(filename, size=None, scale=None):
@@ -33,3 +38,33 @@ def normalize_batch(batch):
     std = batch.new_tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
     batch = batch.div_(255.0)
     return (batch - mean) / std
+
+def denormalize(tensors):
+    """ Denormalizes image tensors using mean and std """
+    for c in range(3):
+        tensors[:, c].mul_(std[c]).add_(mean[c])
+    return tensors
+
+
+def deprocess(image_tensor):
+    """ Denormalizes and rescales image tensor """
+    image_tensor = denormalize(image_tensor)[0]
+    image_tensor *= 255
+    image_np = torch.clamp(image_tensor, 0, 255).cpu().numpy().astype(np.uint8)
+    image_np = image_np.transpose(1, 2, 0)
+    return image_np
+
+
+def extract_frames(video_path):
+    """ Extracts frames from video """
+    frames = []
+    video = av.open(video_path)
+    for frame in video.decode(0):
+        yield frame.to_image()
+
+
+def style_transform(image_size=None):
+    """ Transforms for style image """
+    resize = [transforms.Resize(image_size)] if image_size else []
+    transform = transforms.Compose(resize + [transforms.ToTensor(), transforms.Normalize(mean, std)])
+    return transform
